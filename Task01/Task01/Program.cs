091f5fb5dd6c4e46174f6ece41da43eb3b3ab415
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-09-23 13:27:05 +0500

    1 задание выполнено

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace zadanie1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Осень");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Листья в поле пожелтели,\r\nИ кружатся и летят;\r\nЛишь в бору поникши ели\r\nЗелень мрачную хранят.\r\nПод нависшею скалою\r\nУж не любит, меж цветов,\r\nПахарь отдыхать порою\r\nОт полуденных трудов.\r\nЗверь, отважный, поневоле\r\nСкрыться где-нибудь спешит.\r\nНочью месяц тускл, и поле\r\nСквозь туман лишь серебрит.");
+
+            Console.ReadKey();
+        }
+    }
+}
