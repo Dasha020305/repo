d7aa590bae3fe61d7f33beb7fddac4368c5c1258
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-09-23 13:10:42 +0500

    выполнено задание 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Она");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Две незабудки, два сапфира —\r\nЕе очей приветный взгляд,\r\nИ тайны горнего эфира\r\nВ живой лазури их сквозят.\r\nЕе кудрей руно златое\r\nВ таком свету, какой один,\r\nИзображая неземное,\r\nСводил на землю Перуджин.");
+
+            Console.ReadKey();
+        }
+    }
+}
