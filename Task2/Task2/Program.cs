3b9ca043f8345fce4bba00b75eafb24443e43a4f
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-09-30 13:47:42 +0500

    2 задание выполнено

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = double.Parse(Console.ReadLine());
+
+            double v = (a*a*a);
+            double d = (a * Math.Sqrt(3));
+
+            Console.WriteLine("Объем куба: " + Math.Round(v, 3));
+            Console.WriteLine("Длина диагонали: " + Math.Round(d, 3));
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
