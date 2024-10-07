4cb9aabeabc9aa2175292e854f5bc70d07019c5e
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-10-07 13:40:11 +0500

    Задание 4 выполнено

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyFunction(x);
+
+            Console.WriteLine("f(x) = " + y);
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            return Math.Sqrt((2 * x + Math.Sin(Math.Abs(3 * x))) / 3.56);
+        }
+    }
+}
