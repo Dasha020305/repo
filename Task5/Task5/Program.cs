03d87149c39a70c7527c4704da194cf57d75563e
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-10-13 21:17:50 +0500

    Задание 5 выполнено

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = F(2, F(3, F(4, Math.Sqrt(5))));
+            Console.WriteLine("x = " + Math.Round(y, 3));
+
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b)
+        {
+            return Math.Sqrt(a + b);
+        }
+    }
+}
