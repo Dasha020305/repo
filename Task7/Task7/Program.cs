2da5878893d750d1411d235f01b28651ff18af43
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-10-21 13:05:16 +0500

    Здание 7.1 выполнено

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = GetNumber("n");
+
+            if (IsStatementTrue(n))
+                Console.WriteLine("Утверждение истинно");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+        }
+
+        static bool IsStatementTrue (int n)
+        {
+            return n % 5 ==0 && n % 7 != 0;
+        }
+        static int GetNumber (string numberName)
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
