72f1c79ac5b0f7489bb1c926d33c36dfeab02a82
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-12-09 14:42:56 +0500

    Задание 10.6 выполнено

---
+++ Task10.5/Task10.5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static double F(double x, double a, double b, double c)
+    {
+        return x * x * x + a * x * x + b * x + c;
+    }
+
+    static bool IsRootFound(double left, double right, double epsilon, double a, double b, double c)
+    {
+        while (right - left > epsilon)
+        {
+            double middle = (left + right) / 2;
+            if (F(middle, a, b, c) == 0)
+            {
+                return true;
+            }
+            else if (F(left, a, b, c) * F(middle, a, b, c) < 0)
+            {
+                right = middle;
+            }
+            else
+            {
+                left = middle;
+            }
+        }
+        return false;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите коэффициенты a, b, c:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите точность e:");
+        double epsilon = Convert.ToDouble(Console.ReadLine());
+
+        int n = 1;
+        while (true)
+        {
+            if (F(-n, a, b, c) < 0)
+            {
+                break;
+            }
+            n++;
+        }
+
+        if (IsRootFound(-n, 0, epsilon, a, b, c))
+        {
+            Console.WriteLine("Корень найден.");
+        }
+        else
+        {
+            Console.WriteLine("Корень не найден.");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Task10.6/Task10.6/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            if (!TryInputNumber("Введите число a", out a))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            int b = 0;
+            if (!TryInputNumber("Введите число b", out b))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (a >= b)
+            {
+                Console.WriteLine("Ошибка: a должно быть меньше b.");
+                Console.ReadKey();
+                return;
+            }
+
+            int k = 0;
+            if (!TryInputNumber("Введите число k", out k))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (k < 2)
+            {
+                Console.WriteLine("Ошибка: k должно быть не менее 2.");
+                Console.ReadKey();
+                return;
+            }
+
+            int counter = 0;
+
+            for (int number = a; number <= b; number++)
+            {
+                int divisorCount = CountDivisors(number);
+                if (divisorCount == k)
+                {
+                    Console.Write($"{number}, ");
+                    counter++;
+                }
+            }
+
+            if (counter == 0)
+                Console.WriteLine($"Натуральных чисел в диапазоне [{a}, {b}] с количеством делителей {k} нет.");
+            else
+                Console.WriteLine("\b\b ");
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+
+            return true;
+        }
+
+        static int CountDivisors(int number)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
