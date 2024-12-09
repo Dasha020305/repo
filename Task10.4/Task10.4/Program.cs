f2acca82015084140a8a1f27c7056abb77cf2ebd
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2024-12-09 14:13:20 +0500

    Задание 10.4 выполнено (позиция числа считается с 1)

---
+++ Task10.4/Task10.4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите натуральное число n:");
+
+            if (!int.TryParse(Console.ReadLine(), out n) || n < 1)
+            {
+                Console.WriteLine("Ошибка ввода. Введите натуральное число больше 0.");
+                Console.ReadKey();
+                return;
+            }
+
+            int maxDigit = -1; 
+            int position = 1; 
+            int maxPosition = -1; 
+
+            int temp = n;
+            int divisor = 1;
+
+            while (temp >= 10)
+            {
+                temp /= 10;
+                divisor *= 10;
+            }
+
+            while (divisor > 0)
+            {
+                int digit = n / divisor;
+                n %= divisor;
+                divisor /= 10;
+
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit;
+                    maxPosition = position; 
+                }
+
+                position++; 
+            }
+
+            if (maxPosition != -1)
+            {
+                Console.WriteLine($"Порядковый номер первой наибольшей цифры: {maxPosition}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
