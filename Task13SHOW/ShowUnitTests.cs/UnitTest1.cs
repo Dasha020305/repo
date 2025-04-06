209ed73fa07104070a54a40feb320050bdd8c178
Daria.Voronkova@urfu.me <Daria.Voronkova@urfu.me>
2025-04-06 13:28:50 +0500

    Задание 13 выполнено

---
+++ Task13SHOW/ShowUnitTests.cs/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Task13SHOW;
+
+namespace ShowUnitTests
+{
+    [TestFixture]
+    public class Show
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var show = CreateTestShow();
+
+            Assert.That(show.Title, Is.EqualTo("Импровизаторы"));
+            Assert.That(show.Host, Is.EqualTo("Стас"));
+            Assert.That(show.Description, Is.EqualTo("Комедийное шоу"));
+            Assert.That(show.Periodicity, Is.EqualTo(ShowPeriodicity.Daily));
+            Assert.That(show.ReleaseTime.ToString("t"), Is.EqualTo("20:00"));
+            Assert.That(show.ReleaseDate, Is.Null);
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var show = CreateTestShow();
+            var info = show.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(2));
+            Assert.That(info[0], Is.EqualTo("Импровизаторы (Ведущий: Стас)"));
+            Assert.That(info[1], Is.EqualTo("Периодичность: ежедневная. Время: 20:00. Описание: Комедийное шоу"));
+        }
+
+        private Task13SHOW.Show CreateTestShow()
+        {
+            return new Task13SHOW.Show("Импровизаторы", "Стас", "Комедийное шоу")
+            {
+                Periodicity = ShowPeriodicity.Daily,
+                ReleaseTime = DateTime.Parse("20:00")
+            };
+        }
+    }
+}
