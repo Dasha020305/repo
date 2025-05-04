29a16d9568c283c4ac0de1299c70b92630b412de
Dasha020305 <milliardari0203@gmail.com>
2025-05-04 19:14:15 +0500

    14 задание выполнено

---
+++ Task_14/Task_14/Film.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_14;
+
+namespace Task_14
+{
+    public class Film : Show
+    {
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Country { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Film(string title, string host, string description, string genre, string director, string country, int releaseYear)
+            : base(title, host, description)
+        {
+            Genre = genre;
+            Director = director;
+            Country = country;
+            ReleaseYear = releaseYear;
+        }
+
+        public override string[] GetInfo()
+        {
+            var info = new String[4];
+            var showInfo = base.GetInfo();
+
+            info[0] = showInfo[0];
+            info[1] = showInfo[1];
+            info[2] = showInfo[2];
+            info[3] = $"Жанр: {Genre}. Режиссер: {Director}. Страна: {Country}. Год выхода на экраны: {ReleaseYear}";
+
+            return info;
+        }
+    }
+}
---
+++ Task_14/Task_14/ShowPeriodicity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    public enum ShowPeriodicity
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        NonPeriodic
+    }
+}
